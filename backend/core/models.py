0870ba4004ad1bb1714c0c5e29aa6162294c96e4
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2023-04-10 13:07:35 +0330

    Alter Model

--- backend/core/models.py
+++ backend/core/models.py
@@ -168,10 +168,10 @@ class Assignment(models.Model):
     subtitle = models.TextField()
     description = models.TextField()
     questions = models.ForeignKey(
-        FileMaterial, on_delete=models.RESTRICT, related_name="questions"
+        FileMaterial, on_delete=models.RESTRICT, related_name="questions", null=True
     )
     solutions = models.ForeignKey(
-        FileMaterial, on_delete=models.RESTRICT, related_name="solutions", blank=True
+        FileMaterial, on_delete=models.RESTRICT, related_name="solutions", null=True
     )
     files = models.ManyToManyField(
         FileMaterial, blank=True, related_name="assignment_files"
