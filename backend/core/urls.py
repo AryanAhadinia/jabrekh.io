f5e4d209558b870dd9fb766b4cfdcd478371d8b9
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2023-04-11 10:13:27 +0330

    Change

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,14 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from .views import ping, SemesterViewSet, PersonViewSet, Instruction, TeachingAssisting
+from .views import ping, SemesterViewSet, PersonViewSet, InstructionViewSet, TeachingAssistingViewSet
 
 
 router = DefaultRouter()
 router.register(r"semester", SemesterViewSet, basename="semester")
 router.register(r"person", PersonViewSet, basename="person")
-router.register(r"instruction", Instruction, basename="instruction")
-router.register(r"teaching_assisting", TeachingAssisting, basename="teaching_assisting")
+router.register(r"instruction", InstructionViewSet, basename="instruction")
+router.register(r"teaching_assisting", TeachingAssistingViewSet, basename="teaching_assisting")
 
 urlpatterns = [
     path('', include(router.urls)),
