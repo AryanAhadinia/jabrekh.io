f4e7fce32513eee5b2babe37c60a439880536ac0
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-04-11 16:05:20 +0430

    add MovingGradientText

---
+++ server/frontend/src/componenets/MovingGradientText.tsx
@@ -0,0 +1,62 @@
+// MovingGradientText.tsx
+import React, { useState } from "react";
+import { Text, Box } from "@chakra-ui/react";
+import { css, keyframes } from "@emotion/react";
+
+interface Props {
+  text: string;
+}
+
+const MovingGradientText: React.FC<Props> = ({ text }) => {
+  const [gradientColors] = useState([
+    "#d05a45", // Color 1
+    "#e4aa42", // Color 2
+    "#3f6ee7", // Color 3
+    "#4a9c80", // Color 4
+  ]);
+
+  const getColorShiftKeyframes = () => {
+    let keyframeStr = "";
+    gradientColors.forEach((color, index) => {
+      const percentage = (index * 100) / gradientColors.length;
+      keyframeStr += `
+        ${percentage}% {
+          color: ${color};
+        }
+      `;
+    });
+    return keyframes`${keyframeStr}`;
+  };
+
+  const ColorShift = getColorShiftKeyframes();
+
+  const letterStyle = (index: number) => {
+    const initialColor = gradientColors[index % gradientColors.length];
+    const animationDelay = (index * 2) % (gradientColors.length * 2);
+    return css`
+      color: ${initialColor};
+      animation: ${ColorShift} ${gradientColors.length * 2}s ease-in-out
+        infinite;
+      animation-delay: ${animationDelay}s;
+    `;
+  };
+
+  const renderLetters = () => {
+    return text.split("").map((letter, index) => (
+      <Text
+        as="span"
+        fontSize="24px"
+        fontWeight="bold"
+        textTransform="uppercase"
+        css={letterStyle(index)}
+        key={`letter-${index}`}
+      >
+        {letter}
+      </Text>
+    ));
+  };
+
+  return <Box>{renderLetters()}</Box>;
+};
+
+export default MovingGradientText;
--- server/frontend/src/componenets/NavBar.tsx
+++ server/frontend/src/componenets/NavBar.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { HStack, Box, Text } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
-
+import MovingGradientText from "./MovingGradientText";
 const NavBar = () => {
   const padding = 10;
   return (
     <HStack p={padding}>
       {/* <Image></Image> */}
-      <Text p={5}>Home</Text>
-      <Text p={5}>Materials</Text>
-      <Text p={5}>Assignments</Text>
+      <MovingGradientText text="LINEAR ALGEBRA" />
+      <Text fontSize="15px" p={5}>
+        Home
+      </Text>
+      <Text fontSize="15px" p={5}>
+        Materials
+      </Text>
+      <Text fontSize="15px" p={5}>
+        Assignments
+      </Text>
       <Box pos="absolute" right={padding}>
         <ColorModeSwitch />
       </Box>
