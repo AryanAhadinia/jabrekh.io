0e0315f65569dcdedc457b8ef35f0ae98522eb93
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2023-04-11 15:51:32 +0330

    remove views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,14 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from .views import ping, SemesterViewSet, PersonViewSet, InstructionViewSet, TeachingAssistingViewSet
+from .views import ping, SemesterViewSet
 
 
 router = DefaultRouter()
 router.register(r"semester", SemesterViewSet, basename="semester")
-router.register(r"person", PersonViewSet, basename="person")
-router.register(r"instruction", InstructionViewSet, basename="instruction")
-router.register(r"teaching_assisting", TeachingAssistingViewSet, basename="teaching_assisting")
 
 urlpatterns = [
     path('', include(router.urls)),
